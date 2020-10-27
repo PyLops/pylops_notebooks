6f40e19561f19405c5cad5a063fa7ddad1aaa13c
Matteo Ravasi <mrava@statoil.com>
2020-10-27 11:43:19 +0100

    Modified setup-ssh.sh

--- developement-distributed/setup-ssh.sh
+++ developement-distributed/setup-ssh.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
-# Setup dask server via ssh
+# Setup dask server via ssh. 
+# Run as ./setup-ssh.sh ${SCHEDULER}
 
-dask-ssh --hostfile hostfile.txt
+dask-ssh --hostfile hostfile.txt --scheduler $1
